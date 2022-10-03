586c1aaea37db51d49ed5402505c6a5e586f9198
Juan José <jujopere@bancolombia.com.co>
2022-10-03 11:51:14 -0500

    reorganización home

--- lib/ui/view/home.dart
+++ lib/ui/view/home.dart
@@ -26,20 +26,41 @@ class _HomeState extends ConsumerState<Home> {
       appBar: AppBar(
         title: const Text('Image Refresh Demo'),
       ),
-      body: Center(
-        child:
-            //Image.network('$_imageUrl+#+$_imageVersion')
-            CachedNetworkImage(
-                placeholder: (context, url) =>
-                    const Center(child: CircularProgressIndicator()),
-                imageUrl: '$_imageUrl+#+$_imageVersion'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _refreshImage,
-        child: const Icon(
-          Icons.update,
-          color: Colors.white,
+      body: Card(
+        elevation: 10,
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              SizedBox(
+                child: ListTile(
+                  leading: SizedBox(
+                      width: 100,
+                      child: Image.asset('assets/images/cat-anime.png')),
+                  title: const Center(child: Text('Get your cat')),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              //Image.network('$_imageUrl+#+$_imageVersion')
+              CachedNetworkImage(
+                  placeholder: (context, url) =>
+                      const Center(child: CircularProgressIndicator()),
+                  imageUrl: '$_imageUrl+#+$_imageVersion'),
+            ],
+          ),
         ),
+      ),
+      floatingActionButton: TextButton(
+        style: TextButton.styleFrom(
+          backgroundColor: Colors.deepPurple,
+          foregroundColor: Colors.white,
+          padding: const EdgeInsets.all(16.0),
+          textStyle: const TextStyle(fontSize: 20),
+        ),
+        onPressed: _refreshImage,
+        child: const Text('Meow'),
       ),
     );
   }
